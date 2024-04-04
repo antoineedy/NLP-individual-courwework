7ee5e4436b16d77d0e38ad0632ddf59a1b5eb9e4
Antoine Edy <antoineedy@outlook.fr>
2024-04-04 17:10:24 +0200

    Update version in fine_tuning_abbr_det.ipynb and refactor Vizualization class

--- vectorization.py
+++ vectorization.py
@@ -12,7 +12,7 @@ class Vectorization:
     def __init__(self, method):
         self.model = None
         self.method = method
-        if method not in ["Word2Vec"]:
+        if method not in ["Word2Vec", "pretrained"]:
             raise NotImplementedError("Vectorization method not implemented")
 
     def fit(self, data):
@@ -20,6 +20,8 @@ class Vectorization:
             self.model = gensim.models.Word2Vec(
                 sentences=data, vector_size=100, window=5, min_count=1
             )
+        elif self.method == "pretrained":
+            pass
 
     def transform(self, data, y):
         new_y = []
@@ -29,8 +31,28 @@ class Vectorization:
             else:
                 new_y.append(t + [0] * (50 - len(t)))
         y = new_y
-        if self.method == "Word2Vec":
-            # max size = 50
+        if self.method == "pretrained":
+            out = []
+            for i, sentence in enumerate(data):
+                temp = []
+                for word in sentence:
+                    temp.append(tokenizer.encode(word, add_special_tokens=False))
+                    if len(temp) == 50:
+                        break
+                if len(temp) < 50:
+                    temp += [[0]] * (50 - len(temp))
+                if len(y[i]) < 50:
+                    y[i] += [0] * (50 - len(y[i]))
+                else:
+                    y[i] = y[i][:50]
+                out.append(temp)
+                if len(temp) != 50:
+                    print("ERROR")
+
+            return torch.tensor(np.array(out), dtype=torch.long), torch.tensor(
+                np.array(y), dtype=torch.long
+            )
+        elif self.method == "Word2Vec":
             out = []
             for i, sentence in enumerate(data):
                 temp = []
@@ -48,15 +70,17 @@ class Vectorization:
                 else:
                     y[i] = y[i][:50]
                 out.append(temp)
-            y_bis = np.zeros_like(y)
-            y_bis = y_bis.tolist()
-            for i in range(len(y)):
-                for j in range(len(y[i])):
-                    temp = [0, 0, 0, 0, 0]
-                    temp[y[i][j]] = 1
-                    y_bis[i][j] = temp
 
-            y = y_bis
+            # Here we convert the y to one hot encoding
+            # y_bis = np.zeros_like(y)
+            # y_bis = y_bis.tolist()
+            # for i in range(len(y)):
+            #     for j in range(len(y[i])):
+            #         temp = [0, 0, 0, 0, 0]
+            #         temp[y[i][j]] = 1
+            #         y_bis[i][j] = temp
+
+            # y = y_bis
 
             return torch.tensor(np.array(out), dtype=torch.float32), torch.tensor(
                 np.array(y), dtype=torch.long
--- vizualization.py
+++ vizualization.py
@@ -3,17 +3,13 @@ import pandas
 from colorama import Back, Style
 
 
-class Vizualization:
-    def __init__(self, method):
-        self.method = method
-
-    def plot(self, words, output):
-        # change the color of the token depending on the output
-        output = output.squeeze().tolist()
-        col = {0: Back.BLACK, 1: Back.GREEN, 2: Back.RED, 3: Back.BLUE, 4: Back.MAGENTA}
-        colors = [col[i] for i in output]
-        words = [word.replace("Ġ", "") for word in words]
-        colors = colors[1:-1]
-        words = words[1:-1]
-        for i, word in enumerate(words):
-            print(colors[i] + word, end=" ")
+def vizu(words, output):
+    # change the color of the token depending on the output
+    output = output.squeeze().tolist()
+    col = {0: Back.BLACK, 1: Back.RED, 2: Back.GREEN, 3: Back.BLUE, 4: Back.MAGENTA}
+    colors = [col[i] for i in output]
+    words = [word.replace("Ġ", "") for word in words]
+    colors = colors[1:-1]
+    words = words[1:-1]
+    for i, word in enumerate(words):
+        print(colors[i] + word, end=" ")
